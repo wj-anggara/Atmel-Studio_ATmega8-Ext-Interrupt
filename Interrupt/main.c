2f1334e358bc31cc145591f447aaac5497ca93d2
Anggara Wijaya <wj.anggara@gmail.com>
2017-02-21 02:29:26 +0700

    Atmel-Studio_ATmega8-Ext-Interrupt
    Default external interrupt

---
+++ Interrupt/main.c
@@ -0,0 +1,32 @@
+/*
+ * Interrupt.c
+ *
+ * Created: 18/02/2017 15:36:59
+ * Author : Anggara Wijaya
+ */ 
+#define F_CPU 8000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int a=0;
+
+ISR(INT1_vect)
+{
+	a++;
+}
+
+int main(void)
+{
+    DDRD &= ~(1<<DD3);						//INPUT
+    PORTD |= (1<<PD3);						//PULL_UP
+    MCUCR |= (1<<ISC11) | (1<<ISC10);		// set INT1 to trigger on Falling
+    GICR |= (1<<INT1);						// Turns on INT1
+    sei();									// turn on interrupts
+	
+    while (1) 
+    {
+    }
+}
+
